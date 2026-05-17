c16cd1d07d2841517ca2d686814c4e24c9544e12
Mitsuaki Wada <49638956+mi-wada@users.noreply.github.com>
2026-05-17 11:54:40 +0900

    setup anomaly budget

--- org/budgets.tf
+++ org/budgets.tf
@@ -4,6 +4,10 @@ variable "notification_email" {
   sensitive   = true
 }
 
+locals {
+  budget_limit = "5.0"
+}
+
 resource "aws_sns_topic" "cost_alerts" {
   name = "aws-cost-alerts-topic"
 }
@@ -17,7 +21,7 @@ resource "aws_sns_topic_subscription" "email_sub" {
 resource "aws_budgets_budget" "monthly_limit" {
   name              = "monthly-cost-budget"
   budget_type       = "COST"
-  limit_amount      = "5.0"
+  limit_amount      = local.budget_limit
   limit_unit        = "USD"
   time_unit         = "MONTHLY"
   time_period_start = formatdate("YYYY-MM-DD_00:00", "${plantimestamp()}")
@@ -36,5 +40,30 @@ resource "aws_budgets_budget" "monthly_limit" {
     threshold_type            = "PERCENTAGE"
     notification_type         = "ACTUAL"
     subscriber_sns_topic_arns = [aws_sns_topic.cost_alerts.arn]
+  }
+}
+
+resource "aws_ce_anomaly_monitor" "account_monitor" {
+  name              = "AWS Services"
+  monitor_type      = "DIMENSIONAL"
+  monitor_dimension = "SERVICE"
+}
+
+resource "aws_ce_anomaly_subscription" "anomaly_alerts" {
+  name             = "anomaly-cost-alerts-subscription"
+  frequency        = "DAILY"
+  monitor_arn_list = [aws_ce_anomaly_monitor.account_monitor.arn]
+
+  threshold_expression {
+    dimension {
+      key           = "ANOMALY_TOTAL_IMPACT_ABSOLUTE"
+      match_options = ["GREATER_THAN_OR_EQUAL"]
+      values        = [local.budget_limit]
+    }
+  }
+
+  subscriber {
+    type    = "EMAIL"
+    address = var.notification_email
   }
 }
