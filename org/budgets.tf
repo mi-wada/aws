7e72aef27a0000c8f6c18c910617a8fad5610888
Mitsuaki Wada <49638956+mi-wada@users.noreply.github.com>
2026-05-17 11:38:14 +0900

    setup budget

--- org/budgets.tf
+++ org/budgets.tf
@@ -1,3 +1,19 @@
+variable "notification_email" {
+  type        = string
+  description = "The email address to receive AWS Budget alerts."
+  sensitive   = true
+}
+
+resource "aws_sns_topic" "cost_alerts" {
+  name = "aws-cost-alerts-topic"
+}
+
+resource "aws_sns_topic_subscription" "email_sub" {
+  topic_arn = aws_sns_topic.cost_alerts.arn
+  protocol  = "email"
+  endpoint  = var.notification_email
+}
+
 resource "aws_budgets_budget" "monthly_limit" {
   name              = "monthly-cost-budget"
   budget_type       = "COST"
@@ -5,4 +21,20 @@ resource "aws_budgets_budget" "monthly_limit" {
   limit_unit        = "USD"
   time_unit         = "MONTHLY"
   time_period_start = formatdate("YYYY-MM-DD_00:00", "${plantimestamp()}")
+
+  notification {
+    comparison_operator       = "GREATER_THAN"
+    threshold                 = 80.0
+    threshold_type            = "PERCENTAGE"
+    notification_type         = "FORECASTED"
+    subscriber_sns_topic_arns = [aws_sns_topic.cost_alerts.arn]
+  }
+
+  notification {
+    comparison_operator       = "GREATER_THAN"
+    threshold                 = 100.0
+    threshold_type            = "PERCENTAGE"
+    notification_type         = "ACTUAL"
+    subscriber_sns_topic_arns = [aws_sns_topic.cost_alerts.arn]
+  }
 }
